fccf50e1c846da0ae328e6530b81ddb10f5938c3
ddot727 <ddot727@gmail.com>
2019-08-16 12:09:39 -0400

    added fetchResources function to useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,36 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-// class ResourceList extends React.Component {
-//   state = {
-//     resources: [],
-//   }
-
-//   async componentDidMount() {
-//     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
-
-//     this.setState({ resources: response.data });
-//   }
-
-//   async componentDidUpdate(prevProps) {
-//     if(prevProps.resourceName !== this.props.resourceName) {
-//       const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
-
-//       this.setState({ resources: response.data });
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <ul>
-//         {this.state.resources.map(resource => (
-//           <li key={resource.id}>{resource.title}</li>
-//         ))}
-//       </ul>
-//     )
-//   }
-// }
-
 const ResourceList = ({ resourceName }) => {
   const [resources, setResources] = useState([]);
 
@@ -39,9 +9,14 @@ const ResourceList = ({ resourceName }) => {
 
     setResources(response.data);
   }
+
+  useEffect(() => {
+    fetchResources(resourceName)
+  }, [resourceName])
+
   return (
     <ul>
-      {this.state.resources.map(resource => (
+      {resources.map(resource => (
         <li key={resource.id}>{resource.title}</li>
       ))}
     </ul>
