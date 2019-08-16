e430eaf172e6d9d43b477d885c25a17756315464
ddot727 <ddot727@gmail.com>
2019-08-16 11:32:12 -0400

    took out old code and turn this.state and setState to it's respective useState functions

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,6 @@ import React, { useState } from 'react';
 
 import ResourceList from './ResourceList';
 
-// class App extends React.Component {
-//   state = {
-//     resourceName: 'posts'
-//   }
-
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
-//         <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
-//         <ResourceList resourceName={this.state.resourceName}/>
-//       </React.Fragment>
-//     )
-//   }
-// }
-
 const App = () => {
   const [resourceName, setResourceName] = useState('post');
 
@@ -28,9 +12,9 @@ const App = () => {
 
   return (
     <React.Fragment>
-      <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
-      <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
-      <ResourceList resourceName={this.state.resourceName} />
+      <button onClick={() => setResourceName('posts')}>Posts</button>
+      <button onClick={() => setResourceName('todos')}>Todos</button>
+      <ResourceList resourceName={resourceName} />
     </React.Fragment>
   )
 
