dad12b3cace723a25f3eaa4a226b21816f9de5c0
ddot727 <ddot727@gmail.com>
2019-08-16 11:24:25 -0400

    turned app.js into a functional component/brought in useState

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,31 @@ import React from 'react';
 
 import ResourceList from './ResourceList';
 
-class App extends React.Component {
-  state = {
-    resourceName: 'posts'
-  }
+// class App extends React.Component {
+//   state = {
+//     resourceName: 'posts'
+//   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
-        <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
-        <ResourceList resourceName={this.state.resourceName}/>
-      </React.Fragment>
-    )
-  }
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
+//         <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
+//         <ResourceList resourceName={this.state.resourceName}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
+      <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
+      <ResourceList resourceName={this.state.resourceName} />
+    </React.Fragment>
+  )
+
+
 }
 export default App;
